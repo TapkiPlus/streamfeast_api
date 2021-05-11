e5368e428ea84c169c50b8d6a439a2af214e588e
dmitry-worker <dzenmassta@gmail.com>
2021-05-11 22:25:54 +0300

    Revert back to standart queue

--- api/apps.py
+++ api/apps.py
@@ -7,14 +7,11 @@ from apscheduler.schedulers.background import BackgroundScheduler
 class ApiConfig(AppConfig):
     name = 'api'
 
-    STARTED = False
-
     def ready(self):
-        if not STARTED:
-            STARTED = True  
-            from .email_client import send_oldest
-            logging.basicConfig()
-            logging.getLogger('apscheduler').setLevel(logging.WARNING)
-            scheduler = BackgroundScheduler()
-            scheduler.add_job(send_oldest, 'interval', seconds=10)
-            scheduler.start()
+        print("[+] Starting tasks executors")
+        from .email_client import send_oldest
+        logging.basicConfig()
+        logging.getLogger('apscheduler').setLevel(logging.WARNING)
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(send_oldest, 'interval', seconds=3)
+        scheduler.start()
