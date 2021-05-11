2f29a5647347411969fe02b515b8438afc64bea6
dmitry-worker <dzenmassta@gmail.com>
2021-05-11 21:23:01 +0300

    naive attempt to control a scheduler

--- api/apps.py
+++ api/apps.py
@@ -2,13 +2,17 @@ import logging
 from django.apps import AppConfig
 from apscheduler.schedulers.background import BackgroundScheduler
 
+STARTED = False
+
 class ApiConfig(AppConfig):
     name = 'api'
 
-    def ready(self): 
-        from .email_client import send_oldest
-        logging.basicConfig()
-        logging.getLogger('apscheduler').setLevel(logging.WARNING)
-        scheduler = BackgroundScheduler()
-        scheduler.add_job(send_oldest, 'interval', seconds=10)
-        scheduler.start()
+    def ready(self):
+        if not STARTED:
+            STARTED = True  
+            from .email_client import send_oldest
+            logging.basicConfig()
+            logging.getLogger('apscheduler').setLevel(logging.WARNING)
+            scheduler = BackgroundScheduler()
+            scheduler.add_job(send_oldest, 'interval', seconds=10)
+            scheduler.start()
