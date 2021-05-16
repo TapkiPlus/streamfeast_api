113d6fead8c72b4cd29962138c37d094445ac0b5
dmitry-worker <dzenmassta@gmail.com>
2021-05-16 19:20:05 +0300

    release branch update

--- streamfeast_api/settings.py
+++ streamfeast_api/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 #fixme: update to streamfest.ru
-SITE_URL = 'http://sf.tagobar.ru'
+SITE_URL = 'https://streamfest.ru'
 BASE_URL = ''
 SECRET_KEY = '$sgr9(w7g-5$1c=ip@0ex52rfyd$i5_8qtk28zi9nwy0br^23('
 
