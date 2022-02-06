c0ea42fd187161a5d6e69d4d982877b4e884ce84
Dmitry Voronov <dmitry.voronov@iohk.io>
2022-02-06 20:47:28 +0300

    Fix for finding order by id

--- api/models.py
+++ api/models.py
@@ -303,9 +303,13 @@ class Order(models.Model):
 
     @staticmethod
     def get_recently_paid(order_id): 
-        since = datetime.utcnow() - timedelta(minutes=1)
-        print(f"Looking for paid order {order_id} paid after {since}", flush=True)
-        return Order.objects.get(id=order_id, when_paid__gt=since)
+        # FIXME: cannot return PAID order because 
+        # modulbank notifies us after 
+        # he returns success to a user
+        # since = datetime.utcnow() - timedelta(minutes=1)
+        # print(f"Looking for paid order {order_id} paid after {since}", flush=True)
+        # return Order.objects.get(id=order_id, when_paid__gt=since)
+        return Order.objects.get(id=order_id)
 
 
     @staticmethod
